a19dcc5ab41224a3a656054adf04e1a53c8edcc8
Neha Bommisetty <146405696+NehaBommisetty07@users.noreply.github.com>
2026-05-02 14:24:07 +0530

    Time: 5 ms (86.18%) | Memory: 47.2 MB (29.46%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -27,11 +27,5 @@ class MinStack {
     public int getMin() {
         return minStack.peek();//-2
     }
-    public static void main(String[] args) {
-        MinStack ms=new MinStack();
-        ms.push(-2);
-        ms.push(0);
-        ms.push(3);
-        System.out.println(ms.getMin());
+   
     }
-}
